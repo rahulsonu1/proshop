28e094dc2e3565ba5b1d78eab9b1b5ebd22ec272
Whiteshark2 <rahulsonu2279@gmail.com>
2024-02-18 22:26:50 +0530

    rating comp

---
+++ frontend/src/Component/Rating.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Rating = ({value,text}) => {
+  return (
+    <div className='rating'>
+      <sapn>
+        <i style={{color:'#a1fa66'}} className={value>=1 ? 'fas fa-star':value>=0.5?'fas fa-star-half-alt' : 'far fa-star'} ></i>
+      </sapn>
+      <sapn>
+        <i style={{color:'#a1fa66'}} className={value>=2 ? 'fas fa-star':value>=1.5?'fas fa-star-half-alt' : 'far fa-star'} ></i>
+      </sapn>
+      <sapn>
+        <i style={{color:'#a1fa66'}} className={value>=3 ? 'fas fa-star':value>=2.5?'fas fa-star-half-alt' : 'far fa-star'} ></i>
+      </sapn>
+      <sapn>
+        <i style={{color:'#a1fa66'}} className={value>=4 ? 'fas fa-star':value>=3.5?'fas fa-star-half-alt' : 'far fa-star'} ></i>
+      </sapn>
+      <sapn>
+        <i style={{color:'#a1fa66'}} className={value>=5 ? 'fas fa-star':value>=4.5?'fas fa-star-half-alt' : 'far fa-star'} ></i>
+      </sapn>  
+      <span>{text && text}</span>  
+    </div>
+  )
+}
+
+export default Rating
