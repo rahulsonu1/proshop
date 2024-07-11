abf03003d4f05ab40f5b22898c747d9170c7d71d
Whiteshark2 <rahulsonu2279@gmail.com>
2024-07-11 19:46:05 +0530

    build frontend

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,13 @@ app.use('/api/upload',uploadRoutes)
 const __dirname=path.resolve()
 app.use('/uploads',express.static(path.join(__dirname,'/uploads')))
 
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'frontend/build')))
+
+    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'frontend','build','index.html')))
+}
+
 app.use(errorHandler.error)
 app.use(errorHandler.errorStatus)
 app.use(express.json())
